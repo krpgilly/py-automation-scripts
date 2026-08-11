5cbeb916a158172a059b33dcc829a6cdbb33fd7f
h-gilly <254709390+h-gilly@users.noreply.github.com>
2026-08-11 01:58:59 +0100

    Functions for scrapers

--- Tesco-vs-Lidl-scraper/lidl_scraper.py
+++ Tesco-vs-Lidl-scraper/lidl_scraper.py
@@ -3,12 +3,15 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-url = "https://www.lidl.co.uk/p/milbona-brie/p10045855"
+def scrape_lidl():
+    
+    url = "https://www.lidl.co.uk/p/milbona-brie/p10045855"
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(url)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
 
-price_element = driver.find_element(By.CLASS_NAME, "ods-price__value")
-print("Price:", price_element.text)
+    price_element = driver.find_element(By.CLASS_NAME, "ods-price__value")
+    price = price_element.text.strip()
 
-driver.quit()
+    driver.quit()
+    return price
--- Tesco-vs-Lidl-scraper/tesco_scraper.py
+++ Tesco-vs-Lidl-scraper/tesco_scraper.py
@@ -1,26 +1,19 @@
-# TODO: open browser
-# TODO: navigate to Tesco product page
-# TODO: scrape product name
-# TODO: scrape product price
-# TODO: scrape product weight
-# TODO: normalize price to £/100g
-# TODO: return data
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+def scrape_tesco():
+    url = "https://www.tesco.com/shop/en-GB/products/299914515"
 
-url = "https://www.tesco.com/shop/en-GB/products/299914515"
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(url)
+    price_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "p[class*='priceText']"))
+ )
+    price = price_element.text.strip()
 
-price_element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "p[class*='priceText']"))
-)
-print("Price:", price_element.text)
-
-driver.quit()
+    driver.quit()
+    return price
