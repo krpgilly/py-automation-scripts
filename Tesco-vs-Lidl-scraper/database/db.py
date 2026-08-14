fd5411bfa9b03dd79e02fbc45cc56a6361030fe4
h-gilly <254709390+h-gilly@users.noreply.github.com>
2026-08-14 01:47:53 +0100

    Add raw_price and raw_unit_price columns to prices table

--- Tesco-vs-Lidl-scraper/database/db.py
+++ Tesco-vs-Lidl-scraper/database/db.py
@@ -4,41 +4,40 @@ def init_db():
     conn = sqlite3.connect("products.db")
     cur = conn.cursor()
 
-    cur.execute(
-        """
+    cur.execute("""
         CREATE TABLE IF NOT EXISTS stores (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT UNIQUE
         );
-    """
-    )
+    """)
 
-    cur.execute(
-        """
+    cur.execute("""
         CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT UNIQUE
         );
-    """
-    )
+    """)
 
-    cur.execute(
-        """
+    cur.execute("""
         CREATE TABLE IF NOT EXISTS prices (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             store_id INTEGER,
             product_id INTEGER,
+
+            raw_price TEXT,
+            raw_unit_price TEXT,
             price_per_100g REAL,
+
             scraped_at TEXT,
+
             FOREIGN KEY (store_id) REFERENCES stores(id),
             FOREIGN KEY (product_id) REFERENCES products(id)
         );
-    """
-    )
+    """)
 
     conn.commit()
     conn.close()
 
-
 if __name__ == "__main__":
     init_db()
+
